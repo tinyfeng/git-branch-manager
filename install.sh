016a6615642934f0e37afa0b33edc796b35280ac
yanli.hou <yanli.hou@alo7.com>
2018-10-12 10:59:06 +0800

    check link exist

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-chmod +x agt
-ln -s `pwd`/agt /usr/local/bin
+chmod +x git-branch-manager/agt
+[ -L /usr/local/bin/agt ] && echo "/usr/local/bin/agt exist, will relink"
+ln -sf `pwd`/git-branch-manager/agt /usr/local/bin
